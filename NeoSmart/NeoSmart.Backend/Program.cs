9e32c5dedab10d0bf4562ef94275dfe809314be6
HENRY ALONSO MUNOZ CARVAJAL <henryam4@gmail.com>
2023-09-12 14:42:12 -0500

    We complete the actions of creating, editing and deleting countries and the frontend is modified

--- NeoSmart/NeoSmart.Backend/Program.cs
+++ NeoSmart/NeoSmart.Backend/Program.cs
@@ -1,5 +1,8 @@
 using Microsoft.EntityFrameworkCore;
 using NeoSmart.Backend.Data;
+using NeoSmart.Backend.Intertfaces;
+using NeoSmart.Backend.Repositories;
+using NeoSmart.Backend.UnitsOfWork;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,7 +10,8 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<DataContext>(x => x.UseSqlServer("name=LocalConnection"));
-
+builder.Services.AddScoped(typeof(IGenericUnitOfWork<>), typeof(GenericUnitOfWork<>));
+builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
